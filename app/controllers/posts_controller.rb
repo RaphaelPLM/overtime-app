8666d0d6208a278859f4bb854f44c52697153ab8
Raphael Müller <raphaelplmuller@icloud.com>
2021-06-26 16:35:53 -0300

    Refactor post creation and some tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,18 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new(params.require(:post).permit(:date, :rationale))
+        @post = Post.new(post_params)
 
-        @post.save
+        if @post.save
+            redirect_to @post, notice: 'The post was successfully created.'
+        else
+            render :new
+        end
+    end
 
-        redirect_to @post
+    private
+
+    def post_params
+        params.require(:post).permit(:date, :rationale)
     end
 end
