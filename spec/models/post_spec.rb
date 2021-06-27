ed82ec6f8a8eae7a5f87f3eabf7fa4385d910f6f
Raphael Müller <raphaelplmuller@icloud.com>
2021-06-27 13:29:34 -0300

    Implemented user connection to posts + tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "This is a rationale.")
+      user = User.create(email: "test@test.com", password: "123123", password_confirmation: "123123", first_name: "Raphael", last_name: "Müller")
+      login_as(user, :scope => :user)
+      
+      @post = Post.create(date: Date.today, rationale: "This is a rationale.", user_id: user.id)
     end
     
     it "can be created" do
